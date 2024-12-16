c63c0a675eccdf9034ae23661a483c83a909a4a5
Wdboyes13 <modes-recheck-0r@icloud.com>
2024-12-16 09:59:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,10 @@
-import equations as eq
+prov = input("Enter your province code in all caps: ")
+amount = input("Enter your amount: ")
 
-print("Enter province as 2 digit code in lowercase")
-
-
-prov = input("Enter your province: ")
-
-amount = input("Enter the sale amount: ")
-
-
-
-if prov == "bc":
-    print(eq.bc(amount) + float(amount))
-
-if prov == "ab":
-    print(eq.ab(amount) + float(amount))
-    
-if prov == "sk":
-    print(eq.sk(amount) + float(amount))
-
-if prov == "mn":
-    print(eq.mn(amount) + float(amount))
-
-if prov == "on":
-    print(eq.on(amount) + float(amount))
-
-if prov == "qc":
-    print(eq.qc(amount) + float(amount))  
-
-if prov == "nl":
-    print(eq.nl(amount) + float(amount)) 
-
-if prov == "pei":
-    print(eq.pei(amount) + float(amount)) 
-
-if prov == "ns":
-    print(eq.ns(amount) + float(amount))
-
-if prov == "nb":
-    print(eq.nb(amount) + float(amount))
-
-if prov == "nu":
-    print(eq.nu(amount) + float(amount))
-
-if prov == "yt":
-    print(eq.yt(amount) + float(amount))
-
-if prov == "nt":
-    print(eq.nt(amount) + float(amount))
+rates = {
+    "BC": 0.12, "AB": "0.05", "MN": 0.12, "NB": 0.15,
+    "NL":0.15, "NT": 0.05, "NS": 0.15, "NU": 0.05,
+    "ON": 0.13, "QC": 0.14975, "PEI": 0.15, "SK": 0.05, "YT": 0.05
+}
+total = float(amount) * rates[prov] + int(amount)
+print("Total is:: ", total)
