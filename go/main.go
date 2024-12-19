f1c6cdca0b655b083572d6ea9cfde5c16e19f5c0
Wdboyes13 <modes-recheck-0r@icloud.com>
2024-12-18 17:22:28 -0800

    Update main.go

--- go/main.go
+++ go/main.go
@@ -13,7 +13,7 @@ func main() {
 		"ab": 0.05, "bc": 0.12, "mn": 0.12,
 		"nb": 0.15, "nl": 0.15, "ns": 0.15, "nu": 0.05,
 		"on": 0.13, "qc": 0.14975, "pei": 0.15, "nt": 0.05,
-		"sk": 0.05, "yt": 0.05}
+		"sk": 0.11, "yt": 0.05}
 	var total int = amount*int(rates[prov]) + amount
 	fmt.Print(total)
 	fmt.Print("Ignore the percent sign")
